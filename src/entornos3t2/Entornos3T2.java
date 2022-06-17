37fec60982c238a259bc9e80394238eaf8802487
Paco Elviro <pacoep6@gmail.com>
2022-06-17 13:29:06 +0200

    Creación del proyecto
    creación de gitignore
    
    Grueso de la funcionalidad

---
+++ src/entornos3t2/Entornos3T2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entornos3t2;
+
+/**
+ * Clase principal, sólo sirve para crear objetos y probar
+ * @author PACO
+ */
+public class Entornos3T2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
